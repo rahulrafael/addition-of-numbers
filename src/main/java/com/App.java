fe7d3def0ad0e3a36ba50d9ae1ec643404878832
rahulrafael <nagulapallyrahul3@gmail.com>
2025-04-06 07:50:30 +1000

    Create App.java

---
+++ src/main/java/com/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
